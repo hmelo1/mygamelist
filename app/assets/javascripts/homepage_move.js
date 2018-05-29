f3a568a89cff35dbb46f455550e19b14f9a630e2
Penguino <heriberto@melo.nyc>
2018-05-29 11:37:55 -0400

    Testing button features

---
+++ app/assets/javascripts/homepage_move.js
@@ -0,0 +1,11 @@
+$(function(){
+  $(".js-prev-games").on("click", function(event){
+    alert("TESTING")
+  })
+})
+
+$(function(){
+  $(".js-next-games").on("click", function(event){
+    alert("TESTING")
+  })
+})
