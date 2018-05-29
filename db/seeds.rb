69be0e34a835a74aa1d4a1c0523afe462b88a802
Penguino <heriberto@melo.nyc>
2018-05-29 12:08:53 -0400

    Added few more games to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Game.create(title: "Super Mario Bros.", platforms: "NES", description: "2D Side-Scrolling Platformer", release_year: 1985, genre: "Platformer")
+Game.create(title: "Super Mario Bros. 2", platforms: "NES", description: "2D Side-Scrolling Platformer", release_year: 1988, genre: "Platformer")
+Game.create(title: "Super Mario World", platforms: "NES", description: "2D Side-Scrolling Platformer", release_year: 1990, genre: "Platformer")
+Game.create(title: "Super Mario World 2: Yoshi's Island", platforms: "SNES", description: "2D Side-Scrolling Platformer", release_year: 1995, genre: "Platformer")
+Game.create(title: "Super Mario Bros. 3", platforms: "NES", description: "2D Side-Scrolling Platformer", release_year: 1988, genre: "Platformer")
