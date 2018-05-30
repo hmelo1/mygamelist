ae28e37005ff3a3e4d3860528483a52e49e2751b
Penguino <heriberto@melo.nyc>
2018-05-30 08:05:13 -0400

    Added comments class

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_30_005037) do
+ActiveRecord::Schema.define(version: 2018_05_30_120445) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "game_id"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["game_id"], name: "index_comments_on_game_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "gamelists", force: :cascade do |t|
     t.integer "game_id"
