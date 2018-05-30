4cb9180cadca56560ca43b145ff02b0db1f90656
Penguino <heriberto@melo.nyc>
2018-05-29 20:53:12 -0400

    migrated screenshots

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_28_190111) do
+ActiveRecord::Schema.define(version: 2018_05_30_005037) do
 
   create_table "gamelists", force: :cascade do |t|
     t.integer "game_id"
@@ -34,6 +34,17 @@ ActiveRecord::Schema.define(version: 2018_05_28_190111) do
     t.string "genre"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "screenshots", force: :cascade do |t|
+    t.integer "game_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
+    t.index ["game_id"], name: "index_screenshots_on_game_id"
   end
 
   create_table "users", force: :cascade do |t|
