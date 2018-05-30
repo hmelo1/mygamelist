21e0060e63da5efce7d883b453941c43fe1db651
Penguino <heriberto@melo.nyc>
2018-05-30 14:18:24 -0400

    Adjusting jquery to also change comments over

--- app/assets/javascripts/homepage_move.js
+++ app/assets/javascripts/homepage_move.js
@@ -4,17 +4,17 @@ $(function(){
     var id = parseInt($(".col-md-9").attr("data-id"))-1
     $.get("/games/" + id + "/prev.json", function(data){
       $("a.btn.btn-default.btn-block").attr('href', "/games/" + data["id"] + "/screenshot_upload")
-      $(".title").text(data["title"]);
-      $(".platform").text(data["platforms"]);
-      $(".genre").text(data["genre"]);
-      $(".release_year").text(data["release_year"]);
-      $(".game-title").text(data["title"]);
-      $(".description").text(data["description"]);
-      $(".col-md-9").attr("data-id", data["id"]);
+      $(".title").text(data.game.title);
+      $(".platform").text(data.game.platforms);
+      $(".genre").text(data.game.genre);
+      $(".release_year").text(data.game.release_year);
+      $(".game-title").text(data.game.title);
+      $(".description").text(data.game.description);
+      $(".col-md-9").attr("data-id", data.game.id);
+      debugger
     })
   })
 })
-
 
 $(function(){
   $(".js-next-games").on("click", function(event){
@@ -44,6 +44,7 @@ $(function(){
         let comment = new Comment(response);
         comment.renderComments();
         $(".commentBox").val("");
+        //rails disabled button after submit, this removes and enables submitting.
         $("input.btn.btn").removeAttr('data-disable-with')
         $("input.btn.btn").prop('disabled', false)
       }
