5522ecb9d31642ce62eb743f9cdca8479c874846
Penguino <heriberto@melo.nyc>
2018-05-29 23:05:58 -0400

    Adjusting sizing on screenshots

--- app/models/screenshot.rb
+++ app/models/screenshot.rb
@@ -1,7 +1,7 @@
 class Screenshot < ApplicationRecord
   belongs_to :game
 
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :image, styles: { medium: "150x150>", thumb: "100x100>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
 end
