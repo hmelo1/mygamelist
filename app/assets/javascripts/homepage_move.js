45acd5245be8786f17b18ca9b23689c6d57bde6d
Penguino <heriberto@melo.nyc>
2018-05-29 20:03:34 -0400

    Ajax on show page is working!

--- app/assets/javascripts/homepage_move.js
+++ app/assets/javascripts/homepage_move.js
@@ -3,14 +3,29 @@ $(function(){
     event.preventDefault();
     var id = parseInt($(".col-md-9").attr("data-id"))-1
     $.get("/games/" + id + "/prev.json", function(data){
-          debugger
+          $(".title").text(data["title"]);
+          $(".platform").text(data["platforms"]);
+          $(".genre").text(data["genre"]);
+          $(".release_year").text(data["release_year"]);
+          $(".game-title").text(data["title"]);
+          $(".description").text(data["description"]);
+          $(".col-md-9").attr("data-id", data["id"]);
     })
   })
 })
 
-
 $(function(){
   $(".js-next-games").on("click", function(event){
     event.preventDefault();
+    var id = parseInt($(".col-md-9").attr("data-id"))+1
+    $.get("/games/" + id + "/next.json", function(data){
+          $(".title").text(data["title"]);
+          $(".platform").text(data["platforms"]);
+          $(".genre").text(data["genre"]);
+          $(".release_year").text(data["release_year"]);
+          $(".game-title").text(data["title"]);
+          $(".description").text(data["description"]);
+          $(".col-md-9").attr("data-id", data["id"]);
+    })
   })
 })
