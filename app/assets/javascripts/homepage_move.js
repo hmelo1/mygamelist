45671254f0b130be1a34c1fe674a4deb8d651e05
Penguino <heriberto@melo.nyc>
2018-05-30 07:55:44 -0400

    ajax alos changes screenshot upload now

--- app/assets/javascripts/homepage_move.js
+++ app/assets/javascripts/homepage_move.js
@@ -3,13 +3,15 @@ $(function(){
     event.preventDefault();
     var id = parseInt($(".col-md-9").attr("data-id"))-1
     $.get("/games/" + id + "/prev.json", function(data){
-          $(".title").text(data["title"]);
-          $(".platform").text(data["platforms"]);
-          $(".genre").text(data["genre"]);
-          $(".release_year").text(data["release_year"]);
-          $(".game-title").text(data["title"]);
-          $(".description").text(data["description"]);
-          $(".col-md-9").attr("data-id", data["id"]);
+      debugger
+      $("a.btn.btn-default.btn-block").attr('href', "/games/" + data["id"] + "/screenshot_upload")
+      $(".title").text(data["title"]);
+      $(".platform").text(data["platforms"]);
+      $(".genre").text(data["genre"]);
+      $(".release_year").text(data["release_year"]);
+      $(".game-title").text(data["title"]);
+      $(".description").text(data["description"]);
+      $(".col-md-9").attr("data-id", data["id"]);
     })
   })
 })
@@ -19,13 +21,14 @@ $(function(){
     event.preventDefault();
     var id = parseInt($(".col-md-9").attr("data-id"))+1
     $.get("/games/" + id + "/next.json", function(data){
-          $(".title").text(data["title"]);
-          $(".platform").text(data["platforms"]);
-          $(".genre").text(data["genre"]);
-          $(".release_year").text(data["release_year"]);
-          $(".game-title").text(data["title"]);
-          $(".description").text(data["description"]);
-          $(".col-md-9").attr("data-id", data["id"]);
+      $("a.btn.btn-default.btn-block").attr('href', "/games/" + data["id"] + "/screenshot_upload")
+      $(".title").text(data["title"]);
+      $(".platform").text(data["platforms"]);
+      $(".genre").text(data["genre"]);
+      $(".release_year").text(data["release_year"]);
+      $(".game-title").text(data["title"]);
+      $(".description").text(data["description"]);
+      $(".col-md-9").attr("data-id", data["id"]);
     })
   })
 })
